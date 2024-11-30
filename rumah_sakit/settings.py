7fffd76436ebee2a86b0c68e402ece0f080e2b86
GrowthToBetter <baratrahjaga@gmail.com>
2024-11-30 14:37:57 +0700

    ,

--- rumah_sakit/settings.py
+++ rumah_sakit/settings.py
@@ -48,7 +48,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'cloudinary',
+    'cloudinary_storage',
+
 ]
 
 MIDDLEWARE = [
